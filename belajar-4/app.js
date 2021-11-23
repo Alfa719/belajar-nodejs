d9c8242a40c38ab0f5f7264ff8929a83178083b3
Alfaa719 <nurul.hidayat.f3@gmail.com>
2021-11-23 15:13:00 +0700

    add validator module in belajar-4

--- belajar-4/app.js
+++ belajar-4/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
+const validator = require('validator');
 
 // Buat I/O readline
 const rl = readline.createInterface({
@@ -18,21 +19,25 @@ rl.question("Masukan Nama Anda : ", (name) => {
     rl.question("Masukan No HP : ", (phoneNumber) => {
         // Simpan data ke dalam object
         const data = { name, phoneNumber };
+        if (!validator.isMobilePhone(phoneNumber, 'id-ID')) {
+            console.log("Check Your Phone Number");
+            rl.close();
+        } else {
+            // Ambil data dari data.json dan convert data json ke object
+            const file = fs.readFileSync(dirFile, 'utf-8');
+            const newData = JSON.parse(file);
 
-        // Ambil data dari data.json dan convert data json ke object
-        const file = fs.readFileSync(dirFile, 'utf-8');
-        const newData = JSON.parse(file);
+            // Push / tambahkan data
+            newData.push(data);
 
-        // Push / tambahkan data
-        newData.push(data);
-
-        // Rewrite data up-to-date
-        fs.writeFileSync(dirFile, JSON.stringify(newData, null, 2));
-        console.log('========== Register Berhasil! ==========');
-        console.log(`Nama   : ${name}`);
-        console.log(`No HP  : ${phoneNumber}`);
-        console.log('Contact has added!');
-        console.log('========================================');
-        rl.close();
+            // Rewrite data up-to-date
+            fs.writeFileSync(dirFile, JSON.stringify(newData, null, 2));
+            console.log('========== Register Berhasil! ==========');
+            console.log(`Nama   : ${name}`);
+            console.log(`No HP  : ${phoneNumber}`);
+            console.log('Contact has added!');
+            console.log('========================================');
+            rl.close();
+        }
     })
 });
